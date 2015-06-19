bc9a3c7f886928fbb9621c715d4c98eceae8aef3
Keijiro Takahashi <keijiro@ZX.local>
2015-06-19 14:22:41 +0900

    Initial commit.

---
+++ Assets/TestMenu.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+public class TestMenu : MonoBehaviour
+{
+    public GameObject opaqueSet;
+    public GameObject shadowSet;
+    public GameObject testCard;
+
+    bool showShadowsOnly;
+    bool showTestCard;
+
+    void OnGUI()
+    {
+        if (GUI.Button(new Rect(0, 0, 200, 60), "Switch Shader"))
+        {
+            showShadowsOnly = !showShadowsOnly;
+            opaqueSet.SetActive(!showShadowsOnly);
+            shadowSet.SetActive(showShadowsOnly);
+        }
+
+        if (GUI.Button(new Rect(0, 80, 200, 60), "Test Card"))
+        {
+            showTestCard = !showTestCard;
+            testCard.SetActive(showTestCard);
+        }
+    }
+}
